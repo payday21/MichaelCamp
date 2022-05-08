d8eab077b05c9f94a7a814a280f2f04a691db95d
payday21 <michaelalleyne28@live.com>
2022-05-07 21:51:31 -0400

    endofday

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # MichaelCamp
-this is the 
--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const { campgroundSchema } = require('./schemas.js')
 const mongoose = require('mongoose');
 const methodOverRide = require('method-override');
 const Campground = require('./models/campground');
+const Review = require('./models/review')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -62,6 +63,15 @@ app.get('/campgrounds/:id',catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', { campground });
 }));
+
+app.post('/campgrounds/:id/reviews', catchAsync(async(req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review)
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+}))
 app.get('/campgrounds/:id/edit',catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/edit', { campground });
